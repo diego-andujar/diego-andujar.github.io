38b39bd200899d33041cfc134c4acb6c7ac54684
Diego Andujar <diego.andujar@correo.unimet.edu.ve>
2021-02-09 19:38:43 -0400

    contacto y eso

--- main.js
+++ main.js
@@ -26,3 +26,7 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+function enviar(){
+    let mensaje = alert("mensaje enviado")
+}
